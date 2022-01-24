5826cd4a2308032f06a6b6114d716966d79bc02d
AleksHNZ <86948893+AleksHNZ@users.noreply.github.com>
2022-01-24 19:47:49 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
+const imgStyle = {
+  borderRadius: '20px',
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+
+    <div className='container'>
+
+      <div className='wrapper'>
+
+         <div className='logo'>
+             <img className='main-image' src={require('./images/image-qr-code.png')} style={imgStyle}  />
+          </div>
+        <main className='main-txt'>
+            <h2 className='main-header'>Improve your front-end skills by building projects</h2>
+             <p className='main-para'>
+            Scan the QR code to visit my GitHub repository to check my projects!
+          </p>
+        </main>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
